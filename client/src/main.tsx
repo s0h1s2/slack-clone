6fd40a576edc1ecec93a83ee0748aa9d498c265c
shkar <shkarhsardar@gmail.com>
2024-10-03 21:01:28 +0300

    fix react fragment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { routeTree } from './routeTree.gen'
-import { createRouter, RouterProvider } from '@tanstack/react-router'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { routeTree } from "./routeTree.gen";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 // Register the router instance for type safety
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <React.Fragment>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  </React.Fragment>
+);
