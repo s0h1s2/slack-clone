bdf433c87b26e50b437b6c1d12cd8f3ea627d60f
shkar <shkarhsardar@gmail.com>
2024-11-09 20:30:11 +0300

    added generated client

--- client/src/api/models/index.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './CreateUserRequest';
-export * from './CreateUserResponse';
-export * from './ProblemDetails';
-export * from './ValidationErrorsResponse';
