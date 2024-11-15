886cf203401fef0e8fbcafc4b233df5141e63f38
shkar <shkarhsardar@gmail.com>
2024-11-15 19:48:54 +0300

    fix: make sure error keys are small

---
+++ server/Util/FluentValidationErrorsFactory.cs
@@ -0,0 +1,27 @@
+using System.IO.Pipes;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Results;
+
+namespace server.Util;
+
+public class FluentValidationErrorsFactory:IFluentValidationAutoValidationResultFactory
+{
+    public IActionResult CreateActionResult(ActionExecutingContext context, ValidationProblemDetails? validationProblemDetails)
+    {
+        var errors=validationProblemDetails.Errors.Select(pair => new KeyValuePair<string, string[]>(pair.Key.ToLower(),pair.Value))
+            .ToDictionary(t => t.Key.ToLower(), t => t.Value);
+        var validationResult = new ValidationProblemDetails
+        {
+            Errors = errors,
+            Title = validationProblemDetails.Title,
+            Detail = validationProblemDetails.Detail,
+            Extensions = validationProblemDetails.Extensions,
+            Instance = validationProblemDetails.Instance,
+            Status = validationProblemDetails.Status,
+            Type = validationProblemDetails.Type
+        };
+        return new BadRequestObjectResult(validationResult);
+    }
+}
