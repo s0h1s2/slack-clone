0d9e02aa6669691919fea870ca53fc6b60d4a44b
shkar <shkarhsardar@gmail.com>
2024-12-10 21:14:48 +0300

    feat: create workspace and redirect

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,11 +1,31 @@
 import {UsersApi, WorkspacesApi} from "@/api/apis";
-import {Configuration, ConfigurationParameters} from "@/api/runtime.ts";
-
+import {Configuration, ConfigurationParameters, FetchParams, Middleware, RequestContext} from "@/api/runtime.ts";
+const authMiddleware: Middleware = {
+    pre(context: RequestContext): Promise<FetchParams | void> {
+        const token=localStorage.getItem("token");
+        if(!token) return Promise.reject();
+        return Promise.resolve({
+            url:context.url,
+            init:{
+                ...context.init,
+                headers:{
+                    "Authorization":`Bearer ${token}`,
+                    "Content-Type": "application/json"
+                }
+            }
+        })
+    }
+}
 const configuration: ConfigurationParameters = {
     basePath: "http://localhost:8000",
-    middleware:[]
+    headers:{
+      "Content-Type": "application/json",  
+    },
+    middleware:[authMiddleware]
 }
+
 const apiConfig=new Configuration(configuration);
+
 export const apiClient={
     usersApi:new UsersApi(apiConfig),
     workspaceApi:new WorkspacesApi(apiConfig)
