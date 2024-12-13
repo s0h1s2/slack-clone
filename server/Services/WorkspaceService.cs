ba44a80a9ba76f3bc6189177bb7e5f39187045fc
shkar <shkarhsardar@gmail.com>
2024-12-13 16:24:54 +0300

    fix: added current user as member when creating a workspace

--- server/Services/WorkspaceService.cs
+++ server/Services/WorkspaceService.cs
@@ -16,23 +16,32 @@ public class WorkspaceService
 
     public async Task<GetWorkspaceResponse?> GetWorkspace(int id)
     {
-       var workspace = await  _dbContext.Workspaces.FindAsync(id);
-       return workspace == null ? null : new GetWorkspaceResponse(workspace.Id, workspace.Name,workspace.JoinCode);
+        var workspace = await _dbContext.Workspaces.FindAsync(id);
+        return workspace == null ? null : new GetWorkspaceResponse(workspace.Id, workspace.Name, workspace.JoinCode);
     }
-    public async Task<CreateWorkspaceResponse> CreateWorkspace(CreateWorkspaceRequest request,User user)
+
+    public async Task<CreateWorkspaceResponse> CreateWorkspace(CreateWorkspaceRequest request, User user)
     {
-        // TODO: i'm not sure about this approach need more research.
-       var joiningCode=Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
-       
-       var workspace = new Workspace()
-       {
-           Name = request.Name,
-           JoinCode = joiningCode,
-           UserId = user.Id,
-       };
-       _dbContext.Workspaces.Add(workspace);
-       await _dbContext.SaveChangesAsync();
-       return new CreateWorkspaceResponse(workspace.Id,workspace.Name,joiningCode);
-       
+// TODO: i'm not sure about this approach need more research.
+        var joiningCode = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+
+        var workspace = new Workspace()
+        {
+            Name = request.Name,
+            JoinCode = joiningCode,
+            UserId = user.Id,
+        };
+
+        _dbContext.Workspaces.Add(workspace);
+        await _dbContext.SaveChangesAsync();
+        var member = new WorkspaceMembers()
+        {
+            UserId = user.Id,
+            Role = WorkspaceUserRole.Admin,
+            WorkspaceId = workspace.Id
+        };
+        _dbContext.WorkspaceMembers.Add(member);
+        await _dbContext.SaveChangesAsync();
+        return new CreateWorkspaceResponse(workspace.Id, workspace.Name, joiningCode);
     }
 }
--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ecc0c9f35171356c9570bea55606f58a67be3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6ba85606d120bbe194afe5b448cfa8d484f2cef")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
