ed67f6463e4ee551ee39aa292212a9a4ae36f89a
shkar <shkarhsardar@gmail.com>
2024-12-16 20:22:33 +0300

    feat: added workspace perference

--- client/src/features/workspace/components/PreferenceModal.tsx
+++ client/src/features/workspace/components/PreferenceModal.tsx
@@ -4,6 +4,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
+import { Trash } from "lucide-react";
 import { useState } from "react";
 type Props = {
   open: boolean;
@@ -18,7 +19,25 @@ const PreferenceModal = ({ initialValue, open, setOpen }: Props) => {
         <DialogHeader className="p-4 border-b bg-white">
           <DialogTitle>{value}</DialogTitle>
         </DialogHeader>
-        <div className="px-4 pb-4 flex flex-col"></div>
+        <div className="px-4 pb-4 flex flex-col gap-2">
+          <div className="px-5 py-4 bg-white rounded-lg border-cursor hover:bg-gray-50">
+            <div className="flex items-center justify-between">
+              <p className="text-sm font-semibold">Workspace Name</p>
+              <p className="text-sm text-[#1264a3] hover:underline font-semibold cursor-pointer">
+                Edit
+              </p>
+            </div>
+            <p className="text-sm">{value}</p>
+          </div>
+          <button
+            disabled={false}
+            onClick={() => {}}
+            className="flex items-center gap-x-2 px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50 text-rose-600"
+          >
+            <Trash className="size-4" />
+            <p className="text-sm font-semibold">Delete workspace</p>
+          </button>
+        </div>
       </DialogContent>
     </Dialog>
   );
