e48fa4169d047d9ac27e2cab3d660ec04b4bdc06
shkar <shkarhsardar@gmail.com>
2024-12-18 21:08:34 +0300

    fix: added migrations

---
+++ server/Migrations/20241218172523_add-channel-relation-to-workspace.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class addchannelrelationtoworkspace : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WorkspaceId",
+                table: "WorkspaceChannels",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkspaceChannels_WorkspaceId",
+                table: "WorkspaceChannels",
+                column: "WorkspaceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkspaceChannels_Workspaces_WorkspaceId",
+                table: "WorkspaceChannels",
+                column: "WorkspaceId",
+                principalTable: "Workspaces",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkspaceChannels_Workspaces_WorkspaceId",
+                table: "WorkspaceChannels");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkspaceChannels_WorkspaceId",
+                table: "WorkspaceChannels");
+
+            migrationBuilder.DropColumn(
+                name: "WorkspaceId",
+                table: "WorkspaceChannels");
+        }
+    }
+}
