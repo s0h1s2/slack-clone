2e7878aff8907ce29790cb91c85098b59d78b95b
Shkar <shkarhsardar@gmail.com>
2025-01-04 20:38:05 +0300

    improve toolbar visiability

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -1,11 +1,12 @@
 import Quill, { Delta, Op, QuillOptions } from "quill"
 import "quill/dist/quill.snow.css"
-import {  RefObject, useEffect, useLayoutEffect, useRef, useState } from "react"
+import { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react"
 import { PiTextAa } from "react-icons/pi"
 import { MdSend } from "react-icons/md"
 import { Button } from "./ui/button";
 import { ImageIcon, Smile } from "lucide-react";
 import Hint from "./Hint"
+import { cn } from "@/lib/utils"
 type Message = {
   image: File | null
   text: string
@@ -22,59 +23,88 @@ type Props = {
 
 }
 const Editor = ({ variant = "create", onSubmit, onCancel, onSave, innerRef, placeHolder = "Write something...", defaultValue = [], disabled = false }: Props) => {
-  const [message,setMessage]=useState("");
+  const [message, setMessage] = useState("");
+  const [isToolbarVisable, setToolbarVisiable] = useState(true);
+
   const containerRef = useRef<HTMLDivElement>(null);
-  const submitRef=useRef(onSubmit);
-  const placeHolderRef=useRef(placeHolder);
-  const saveRef=useRef(onSave);
-  const quillRef=useRef<Quill | null>(null);
-  const defaultValueRef=useRef(defaultValue);
-  const disabledRef=useRef(disabled);
+  const submitRef = useRef(onSubmit);
+  const placeHolderRef = useRef(placeHolder);
+  const saveRef = useRef(onSave);
+  const quillRef = useRef<Quill | null>(null);
+  const defaultValueRef = useRef(defaultValue);
+  const disabledRef = useRef(disabled);
   useLayoutEffect(() => {
-    submitRef.current=onSubmit;
-    placeHolderRef.current=placeHolder;
-    saveRef.current=onSave;
-    defaultValueRef.current=defaultValue;
-    disabledRef.current=disabled;
+    submitRef.current = onSubmit;
+    placeHolderRef.current = placeHolder;
+    saveRef.current = onSave;
+    defaultValueRef.current = defaultValue;
+    disabledRef.current = disabled;
   })
   useEffect(() => {
     if (!containerRef.current) return;
     const container = containerRef.current
     const editorContainer = container.appendChild(container.ownerDocument.createElement('div'))
-    const options: QuillOptions = { 
+    const options: QuillOptions = {
       theme: "snow",
-      placeholder:placeHolderRef.current,
+      placeholder: placeHolderRef.current,
+      modules: {
+        keyboard: {
+          bindings: {
+            enter: {
+              key: "Enter",
+              handler: () => {
+                return;
+              }
+            },
+            shift_enter: {
+              key: "Enter",
+              shiftKey: true,
+              handler: () => {
+                quill.insertText(quill.getSelection()?.index || 0, "\n")
+              }
+            }
+          }
+        }
+      }
     };
     const quill = new Quill(editorContainer, options);
-    quillRef.current=quill;
+    quillRef.current = quill;
     quillRef.current.focus();
     quill.setContents(defaultValueRef.current)
     setMessage(quill.getText())
-    quill.on(Quill.events.TEXT_CHANGE,()=>{
+    quill.on(Quill.events.TEXT_CHANGE, () => {
       setMessage(quill.getText())
     })
-    if(innerRef){}
+    if (innerRef) { }
     return () => {
       quill.off(Quill.events.TEXT_CHANGE)
       if (container) {
         container.innerHTML = ""
       }
-      if(quillRef.current){
-        quillRef.current=null;
+      if (quillRef.current) {
+        quillRef.current = null;
       }
 
-      if(innerRef){
-        innerRef.current=null;
+      if (innerRef) {
+        innerRef.current = null;
       }
     }
   }, [innerRef])
+  const toggleToolbar = () => {
+    setToolbarVisiable((current) => !current)
+    const toolbarEl = containerRef.current?.querySelector('.ql-toolbar');
+    if (toolbarEl) {
+      toolbarEl.classList.toggle("hidden");
+    }
+  }
+  const isMessageBoxEmpty = message.trim().length === 0;
   return (
     <div className="flex flex-col">
       <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
         <div ref={containerRef} className="h-full ql-custom" />
         <div className="flex px-2 pb-2 z-[5]">
-          <Hint label="Hide formatting">
-            <Button size="iconSm" variant="ghost" onClick={() => { }}>
+          <Hint label={isToolbarVisable ? "Hide formatting" : "Show formatting"}>
+            <Button size="iconSm" disabled={disabled} variant="ghost" onClick={toggleToolbar}>
               <PiTextAa className="size-4" />
             </Button>
           </Hint>
@@ -91,11 +121,11 @@ const Editor = ({ variant = "create", onSubmit, onCancel, onSave, innerRef, plac
             </Hint>
           }
           {variant == "update" && (<div className="ml-auto flex items-center gap-x-2">
-            <Button variant="outline" size="sm" onClick={() => { }} disabled>Cancel</Button>
-            <Button variant="outline" className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white" size="sm" onClick={() => { }} disabled>Save</Button>
+            <Button disabled={disabled} variant="outline" size="sm" onClick={() => { }} >Cancel</Button>
+            <Button disabled={disabled || isMessageBoxEmpty} variant="outline" className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white" size="sm" onClick={() => { }} >Save</Button>
           </div>)}
           {variant == "create" &&
-            <Button disabled={message.trim().length===0} className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white" size="iconSm" onClick={() => { }}>
+            <Button disabled={isMessageBoxEmpty || disabled} className={cn("ml-auto mt-1", isMessageBoxEmpty ? "bg-white hover:bg-white text-muted-foreground" : "bg-[#007a5a] hover:bg-[#007a5a]/80 text-white")} size="iconSm" onClick={() => { }}>
               <MdSend />
             </Button>
           }
@@ -106,7 +136,7 @@ const Editor = ({ variant = "create", onSubmit, onCancel, onSave, innerRef, plac
           <strong>Shift + Return</strong> to enter new line
         </p>
       </div>
-    </div>
+    </div >
   )
 }
 
