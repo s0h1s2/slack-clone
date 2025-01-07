e7f4dbfd4df905115c0d17cefd262ad1799b1444
Shkar <shkarhsardar@gmail.com>
2025-01-07 20:09:45 +0300

    feat: enhance editor to support image uploads and update message type

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -4,29 +4,29 @@ import { RefObject, useEffect, useLayoutEffect, useRef, useState } from "react"
 import { PiTextAa } from "react-icons/pi"
 import { MdSend } from "react-icons/md"
 import { Button } from "./ui/button";
-import { ImageIcon, Smile } from "lucide-react";
+import { ImageIcon, Smile, XIcon } from "lucide-react";
 import Hint from "./Hint"
 import { cn } from "@/lib/utils"
 import EmojiPopover from "./EmojiPopover"
-type Message = {
+export type ChatMessage = {
   image: File | null
   text: string
 }
 type Props = {
   variant?: "create" | "update"
-  onSubmit: (message: Message) => void,
+  onSubmit: (message: ChatMessage) => void,
   onCancel?: () => void,
-  onSave?: (message: Message) => void
+  onSave?: (message: ChatMessage) => void
   placeHolder?: string
   defaultValue?: Delta | Op[]
   disabled?: boolean
   innerRef?: RefObject<Quill | null>
-
 }
 const Editor = ({ variant = "create", onSubmit, onCancel, onSave, innerRef, placeHolder = "Write something...", defaultValue = [], disabled = false }: Props) => {
   const [message, setMessage] = useState("");
+  const [image, setImage] = useState<File | null>(null);
   const [isToolbarVisable, setToolbarVisiable] = useState(true);
-
+  const imageElRef = useRef<HTMLInputElement | null>(null);
   const containerRef = useRef<HTMLDivElement>(null);
   const submitRef = useRef(onSubmit);
   const placeHolderRef = useRef(placeHolder);
@@ -104,12 +104,31 @@ const Editor = ({ variant = "create", onSubmit, onCancel, onSave, innerRef, plac
       quill.insertText(quill.getSelection()?.index || 0, emoji!.native)
     }
   }
+  
   const isMessageBoxEmpty = message.trim().length === 0;
   return (
+
     <div className="flex flex-col">
-      <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+      <input type="file"  ref={imageElRef} onChange={(event) => setImage(event.target.files![0])} className="hidden" />
+            <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
         <div ref={containerRef} className="h-full ql-custom" />
-        <div className="flex px-2 pb-2 z-[5]">
+      {!!image && (
+        <div className="p-2">
+          <div className="relative size-[62px] flex items-center justify-center group/image">
+              <Hint label="Remove image">
+            <button className="hidden group-hover/image:flex rounded-full bg-black/70 hover:bg-black absolute -top-2.5 -right-2.5 text-white size-6 z-[4] border-2 border-white items-center justify-center" onClick={()=>{
+                setImage(null)
+                imageElRef.current!.value=''
+              }}>
+                <XIcon className="size-3.5" />
+            </button>
+            </Hint>
+              <img src={URL.createObjectURL(image)} alt="Uploaded" className="w-full h-full rounded-xl overflow-hidden border object-cover"/>
+
+          </div>
+        </div>
+      )}
+  <div className="flex px-2 pb-2 z-[5]">
           <Hint label={isToolbarVisable ? "Hide formatting" : "Show formatting"}>
             <Button size="iconSm" disabled={disabled} variant="ghost" onClick={toggleToolbar}>
               <PiTextAa className="size-4" />
@@ -118,22 +137,29 @@ const Editor = ({ variant = "create", onSubmit, onCancel, onSave, innerRef, plac
           <EmojiPopover hint="Emoji" onEmojiSelect={onEmojiSelect}>
             <Button size="iconSm" variant="ghost" >
               <Smile className="size-4" />
-
             </Button>
           </EmojiPopover >
           {variant == "create" &&
             <Hint label="Image">
-              <Button size="iconSm" variant="ghost" onClick={() => { }}>
+              <Button size="iconSm" variant="ghost" onClick={() => imageElRef.current?.click()}>
                 <ImageIcon className="size-4" />
               </Button>
             </Hint>
           }
           {variant == "update" && (<div className="ml-auto flex items-center gap-x-2">
-            <Button disabled={disabled} variant="outline" size="sm" onClick={() => { }} >Cancel</Button>
-            <Button disabled={disabled || isMessageBoxEmpty} variant="outline" className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white" size="sm" onClick={() => { }} >Save</Button>
+            <Button disabled={disabled} variant="outline" size="sm" onClick={onCancel} >Cancel</Button>
+            <Button disabled={disabled || isMessageBoxEmpty} variant="outline" className="bg-[#007a5a] hover:bg-[#007a5a]/80 text-white" size="sm" onClick={
+()=>onSubmit({
+              image:image,
+              text:JSON.stringify(quillRef.current?.getContents())
+            })} >Save</Button>
           </div>)}
           {variant == "create" &&
-            <Button disabled={isMessageBoxEmpty || disabled} className={cn("ml-auto mt-1", isMessageBoxEmpty ? "bg-white hover:bg-white text-muted-foreground" : "bg-[#007a5a] hover:bg-[#007a5a]/80 text-white")} size="iconSm" onClick={() => { }}>
+            <Button disabled={isMessageBoxEmpty || disabled} className={cn("ml-auto mt-1", isMessageBoxEmpty ? "bg-white hover:bg-white text-muted-foreground" : "bg-[#007a5a] hover:bg-[#007a5a]/80 text-white")} size="iconSm" onClick={()=>onSubmit({
+              image:image,
+              text:JSON.stringify(quillRef.current?.getContents())
+            })}>
+
               <MdSend />
             </Button>
           }
--- client/src/features/channel/components/ChatInput.tsx
+++ client/src/features/channel/components/ChatInput.tsx
@@ -1,9 +1,12 @@
-import Editor from "@/components/Editor"
+import Editor, { ChatMessage } from "@/components/Editor"
 
 const ChatInput = () => {
+  const handleSubmit=({text,image}:ChatMessage)=>{
+
+  }
   return (
     <div className="px-5 w-full">
-      <Editor onSubmit={(text)=>console.log(text)}  placeHolder="Say something..."/>
+      <Editor onSubmit={handleSubmit}  placeHolder="Say something..."/>
     </div>
   )
 }
