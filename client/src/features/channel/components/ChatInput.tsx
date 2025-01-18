0d31aedba910471683e7243de9d7b94a2720be83
shkar <shkarhsardar@gmail.com>
2025-01-18 22:11:27 +0300

    feat: update ChatInput component to reset key on message submission for re-rendering

--- client/src/features/channel/components/ChatInput.tsx
+++ client/src/features/channel/components/ChatInput.tsx
@@ -2,20 +2,27 @@ import { ResponseError } from "@/api";
 import { apiClient } from "@/api/client";
 import Editor, { ChatMessage } from "@/components/Editor";
 import { useParams, useRouter } from "@tanstack/react-router";
+import { useState } from "react";
 
 const ChatInput = () => {
   const { channelId } = useParams({
     from: "/workspaces/$workspaceId_/channels/$channelId",
   });
+  const [key, setKey] = useState(1);
   const handleSubmit = async ({ text, image }: ChatMessage) => {
     try {
-      await apiClient.channelsApi.apiChannelIdChatPostRaw({id:channelId,chat:text,attachment:image??null});
+      await apiClient.channelsApi.apiChannelIdChatPostRaw({
+        id: Number(channelId),
+        chat: text,
+        attachment: image ?? undefined,
+      });
+      setKey((prev) => prev + 1);
     } catch (error: ResponseError | Error | unknown) {
       console.error(error);
     }
   };
   return (
-    <div className="px-5 w-full">
+    <div className="px-5 w-full" key={key}>
       <Editor onSubmit={handleSubmit} placeHolder="Say something..." />
     </div>
   );
