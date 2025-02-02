5b9e07c5c2fa0af4c85aca879138bbe8564f76ce
shkar <shkarhsardar@gmail.com>
2025-02-02 21:47:34 +0300

    fix: update message broadcasting to specific channel group in Messages controller

--- server/Controllers/Messages.cs
+++ server/Controllers/Messages.cs
@@ -48,7 +48,8 @@ public class Messages : Controller
         message.UpdateAt = DateTime.UtcNow;
         try
         {
-            await _channelHub.Clients.All.UpdateMessage(message.Id, message.Message);
+            await _channelHub.Clients.Group(message.ChannelId.ToString()).UpdateMessage(message.Id, message.Message);
+
             await _dbContext.SaveChangesAsync();
             return Ok();
         }
