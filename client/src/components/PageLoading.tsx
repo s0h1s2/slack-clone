ad3e1a8dd754f2b82243649373d2307be07d6bbe
shkar <shkarhsardar@gmail.com>
2025-02-07 19:59:37 +0300

    fix: adjust height of PageLoading component and Thread header for better layout consistency

--- client/src/components/PageLoading.tsx
+++ client/src/components/PageLoading.tsx
@@ -1,7 +1,7 @@
 import { LoaderCircle } from "lucide-react";
 const PageLoading = () => {
   return (
-    <div className="min-h-full flex items-center justify-center">
+    <div className="h-full flex items-center justify-center">
       <LoaderCircle className="animate-spin size-12" />
     </div>
   );
